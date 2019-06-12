ac4149843b45d9b7c55e275e0115c9f57faf47fd
vitor <vitor.hoshino1@hotmail.com>
2019-06-12 15:16:48 -0400

    tela de login

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -12,10 +14,48 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
+
+
 	$page = new Page();
 
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin', function() {
+    
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+			"header"=>false,
+			"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->GET('/admin/logout', function(){
+	User::logout();
+	header("Location: /admin/login");
+	exit;
 });
 
 $app->run();
